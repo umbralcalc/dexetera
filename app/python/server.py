36760125fee91cf291100a952a3544b501ce0a0f
umbralcalc <umbralcalc@gmail.com>
2024-07-02 06:44:01 +0100

    Improved protocol

--- app/python/server.py
+++ app/python/server.py
@@ -8,12 +8,12 @@ from .partition_state_pb2 import PartitionState, State
 
 class ActionTaker(Protocol):
     @property
-    def number_of_input_messages(self) -> int:
+    def input_state_map(self) -> dict[int, str]:
         ...
     def take_next_action(
         self, 
         time: float, 
-        states: dict[int, list[float]]
+        states: dict[str, list[float]]
     ) -> list[float]:
         ...
 
@@ -25,10 +25,14 @@ async def _launch_websocket_server(action_taker: ActionTaker):
         async for binary_message in websocket:
             message = PartitionState()
             message.ParseFromString(binary_message)
-            received_messages[message.partition_index] = message.state.values
-            time = message.cumulative_timesteps
-            if len(received_messages) == action_taker.number_of_input_messages:
-                action_state = action_taker.take_next_action(time, received_messages)
+            received_messages[
+                action_taker.input_state_map[message.partition_index]
+            ] = message.state.values
+            if len(received_messages) == len(action_taker.input_state_map):
+                action_state = action_taker.take_next_action(
+                    message.cumulative_timesteps, 
+                    received_messages,
+                )
                 await websocket.send(State(values=action_state).SerializeToString())
                 received_messages.clear()
 
