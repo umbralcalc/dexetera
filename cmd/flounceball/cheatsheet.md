957e4c586762f26f3a6595409e07bb86bf752c49
umbralcalc <umbralcalc@gmail.com>
2024-09-01 22:58:33 +0100

    Upadate to main binary

--- cmd/flounceball/cheatsheet.md
+++ cmd/flounceball/cheatsheet.md
@@ -4,14 +4,5 @@
 
 ## List value meanings for `states["latest_match_state"]`
 
-0. is a [0,1] indicator of the 'restart state', where 1 means play is currently restarting
-1. is a [0,1] indicator of the 'possession state', where 0 means your team is in possession
-2. is your team's total 'air time' score (see the rules for the definition)
-3. is the opposition team's total 'air time' score (see the rules for the definition)
-4. is the cumulative 'air time' for the ball in current possession
-5. is the (horizontal) speed of the ball across the pitch
-6. is the radial position of the ball on the pitch
-7. is the angular position of the ball on the pitch
-8. is the projected radial position that the ball is heading towards
-9. is the projected angular position that the ball is heading towards
-10. is the cumulative time that the ball has spent falling once it reaches the end of its path
+0. is your team's total points score
+1. is the opposition team's total points score
--- cmd/flounceball/main.go
+++ cmd/flounceball/main.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 
 	"golang.org/x/exp/rand"
+	"gonum.org/v1/gonum/stat/distuv"
 
 	"github.com/umbralcalc/dexetera/pkg/examples"
 	"github.com/umbralcalc/dexetera/pkg/simio"
@@ -32,31 +33,24 @@ func main() {
 		partitions,
 		simulator.Partition{Iteration: &simulator.ParamValuesIteration{}},
 	)
-	yourPlayerTemplateParams := simulator.Params{
-		"match_state_partition_index":         {21},
-		"opposition_player_partition_indices": {11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
-		"player_space_finding_talent":         {7},
-		"player_ball_interaction_speed":       {0.1},
-		"player_ball_interaction_inaccuracy":  {0.1},
-		"team_possession_state_value":         {0},
-		"team_attacking_distance_threshold":   {10.0},
-		"team_defensive_distance_threshold":   {10.0},
-		"player_movement_speed":               {0.01},
+	uniformDist := &distuv.Uniform{
+		Min: 0.0,
+		Max: 1.0,
+		Src: rand.NewSource(uint64(rand.Intn(10000))),
 	}
 	index := len(partitions)
-	radius := 0.0
-	angle := 0.0
 	for i := 1; i < 11; i++ {
 		seeds = append(seeds, uint64(rand.Intn(10000)))
-		stateWidths = append(stateWidths, 4)
+		stateWidths = append(stateWidths, 2)
 		stateHistoryDepths = append(stateHistoryDepths, 1)
-		initStateValues = append(initStateValues, []float64{radius, angle, 0.0, 0.0})
-		if i%2 == 0 {
-			radius += examples.PitchRadiusMetres / 5.0
-		}
-		angle += math.Pi
-		copyParams := yourPlayerTemplateParams
-		params = append(params, copyParams)
+		initStateValues = append(
+			initStateValues,
+			[]float64{
+				examples.PitchRadiusMetres * uniformDist.Rand(),
+				2.0 * math.Pi * uniformDist.Rand(),
+			},
+		)
+		params = append(params, simulator.Params{})
 		yourPlayerIteration := &examples.FlounceballPlayerStateIteration{}
 		partitions = append(
 			partitions,
@@ -66,30 +60,18 @@ func main() {
 			strconv.Itoa(i)+"_state"] = index
 		index += 1
 	}
-	otherPlayerTemplateParams := simulator.Params{
-		"match_state_partition_index":         {21},
-		"opposition_player_partition_indices": {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
-		"player_space_finding_talent":         {7},
-		"player_ball_interaction_speed":       {0.1},
-		"player_ball_interaction_inaccuracy":  {0.1},
-		"team_possession_state_value":         {1},
-		"team_attacking_distance_threshold":   {10.0},
-		"team_defensive_distance_threshold":   {10.0},
-		"player_movement_speed":               {0.01},
-	}
-	radius = 0.0
-	angle = math.Pi
 	for i := 1; i < 11; i++ {
 		seeds = append(seeds, uint64(rand.Intn(10000)))
-		stateWidths = append(stateWidths, 4)
+		stateWidths = append(stateWidths, 2)
 		stateHistoryDepths = append(stateHistoryDepths, 1)
-		initStateValues = append(initStateValues, []float64{radius, angle, 0.0, 0.0})
-		if i%2 == 0 {
-			radius += examples.PitchRadiusMetres / 5.0
-		}
-		angle += math.Pi
-		copyParams := otherPlayerTemplateParams
-		params = append(params, copyParams)
+		initStateValues = append(
+			initStateValues,
+			[]float64{
+				examples.PitchRadiusMetres * uniformDist.Rand(),
+				2.0 * math.Pi * uniformDist.Rand(),
+			},
+		)
+		params = append(params, simulator.Params{})
 		otherPlayerIteration := &examples.FlounceballPlayerStateIteration{}
 		partitions = append(
 			partitions,
@@ -100,16 +82,10 @@ func main() {
 		index += 1
 	}
 	seeds = append(seeds, uint64(rand.Intn(10000)))
-	stateWidths = append(stateWidths, 11)
+	stateWidths = append(stateWidths, 2)
 	stateHistoryDepths = append(stateHistoryDepths, 1)
-	initStateValues = append(
-		initStateValues,
-		[]float64{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
-	)
-	matchTemplateParams := simulator.Params{
-		"max_ball_falling_time": {1.0},
-	}
-	params = append(params, matchTemplateParams)
+	initStateValues = append(initStateValues, []float64{0.0, 0.0})
+	params = append(params, simulator.Params{})
 	matchIteration := &examples.FlounceballMatchStateIteration{}
 	partitions = append(
 		partitions,
