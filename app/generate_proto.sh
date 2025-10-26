6ac1f3b4d5a7385446fb5bc9a9c5904866dca237
Robert Hardwick <umbralcalc@gmail.com>
2025-10-26 07:36:32 +0000

    updated script

--- app/generate_proto.sh
+++ app/generate_proto.sh
@@ -2,4 +2,4 @@ protoc -I=. \
     --go_out=./.. \
     --js_out=library=./src/partition_state_pb,binary:. \
     partition_state.proto;
-# protoc --python_out=../../../dexact/dexact partition_state.proto;
+# protoc --python_out=../../dexact/dexact partition_state.proto;
